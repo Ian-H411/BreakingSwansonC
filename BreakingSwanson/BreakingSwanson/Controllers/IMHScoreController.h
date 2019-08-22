0cacf3e6e447f96474e7b864daf61fb656b52f52
Ian_H411 <ian.h411@gmail.com>
2019-08-22 16:09:17 -0600

    final optimization of logic

---
+++ BreakingSwanson/BreakingSwanson/Controllers/IMHScoreController.h
@@ -0,0 +1,17 @@
+//
+//  IMHScoreController.h
+//  BreakingSwanson
+//
+//  Created by Ian Hall on 8/22/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IMHScoreController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BreakingSwanson/BreakingSwanson/Controllers/IMHScoreController.m
@@ -0,0 +1,13 @@
+//
+//  IMHScoreController.m
+//  BreakingSwanson
+//
+//  Created by Ian Hall on 8/22/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+#import "IMHScoreController.h"
+
+@implementation IMHScoreController
+
+@end
